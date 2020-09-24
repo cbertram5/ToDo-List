95b591b2db9ff06e7547b9618db91b08b93f4f5f
Chris Bertram <bertramc@bc.edu>
2020-09-24 13:28:42 -0400

    5.6 complete

---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by Chris Bertram on 9/23/20.
+//  Copyright © 2020 Chris Bertram. All rights reserved.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
+
